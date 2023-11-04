b2157f54e87bd36f5b75b59525127cf867f9890f
j-awwad <jawwad.amd@gmail.com>
2023-11-04 06:34:30 -0700

    added features

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Grid } from "./Components/Grid";
+import Example from "./Components/filter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Grid />
+        <Example />
       </div>
     </>
   );
---
+++ src/Components/Grid.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./grid.css";
+
+export const Grid = () => {
+  return (
+    <div className="containerr">
+      <div className="items-1">items-1</div>
+      <div className="items-2">items-2</div>
+      <div className="items-3">items-3</div>
+      <div className="items-4">items-4</div>
+      <div className="items-5">items-5</div>
+      <div className="items-6">items-6</div>
+      <div className="items-7">items-7</div>
+      <div className="items-8">items-8</div>
+      <div className="items-9">items-9</div>
+
+
+      <div className="item-1"></div>
+      <div className="item-2"></div>
+      <div className="item-3"></div>
+    </div>
+  );
+};
